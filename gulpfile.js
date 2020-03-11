543c42e1664dbf692d0316141f9eb3657fb5452a
Igor-AZ <igor.apfentaler@autozubak.com>
2020-03-11 13:33:51 +0100

    Updatet gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,66 @@
-const gulp = require('gulp');
+// Initialize modules
+// Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
+const { src, dest, watch, series, parallel } = require('gulp');
+// Importing all the Gulp-related packages we want to use
+const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+var replace = require('gulp-replace');
 
-// css compile
-function style() {
-    return gulp.src('assets/style/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('assets/style/'))
-    .pipe(browserSync.stream());
+
+// File paths
+const files = { 
+    scssPath: 'app/scss/**/*.scss',
+    jsPath: 'app/js/**/*.js'
 }
 
-function watch() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    });
-    gulp.watch('assets/style/**/*.scss', style);
-    gulp.watch('./*.html').on('change', browserSync.reload);
-    gulp.watch('assets/js/**/*.js', style).on('change', browserSync.reload);
+// Sass task: compiles the style.scss file into style.css
+function scssTask(){    
+    return src(files.scssPath)
+        .pipe(sourcemaps.init()) // initialize sourcemaps first
+        .pipe(sass()) // compile SCSS to CSS
+        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
+        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+        .pipe(dest('dist')
+    ); // put final CSS in dist folder
 }
 
-exports.style = style;
-exports.watch = watch;
+// JS task: concatenates and uglifies JS files to script.js
+function jsTask(){
+    return src([
+        files.jsPath
+        //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
+        ])
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(dest('dist')
+    );
+}
+
+// Cachebust
+function cacheBustTask(){
+    var cbString = new Date().getTime();
+    return src(['index.html'])
+        .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+        .pipe(dest('.'));
+}
+
+function watchTask(){
+    watch([files.scssPath, files.jsPath],
+        {interval: 1000, usePolling: true},
+        series(
+            parallel(scssTask, jsTask),
+            cacheBustTask
+        )
+    );    
+}
+
+exports.default = series(
+    parallel(scssTask, jsTask), 
+    cacheBustTask,
+    watchTask
+);
